eb052532224bf2141c875560c4a75c35db055888
346791489@qq.com <wyyjd20140302>
2021-08-03 15:35:06 +0800

    1.主链升级

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/network/blockchain/extrinsics/TransferRequestv28.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/network/blockchain/extrinsics/TransferRequestv28.kt
@@ -4,9 +4,14 @@ import jp.co.soramitsu.fearless_utils.scale.EncodableStruct
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
 import jp.co.soramitsu.feature_wallet_impl.data.network.blockchain.struct.SubmittableExtrinsicV28
 
-class TransferRequestV28(extrinsic: EncodableStruct<SubmittableExtrinsicV28>) : RuntimeRequest(
+class TransferRequestV28(extrinsic: EncodableStruct<SubmittableExtrinsicV28>)
+    : RuntimeRequest(
     method = "author_submitExtrinsic",
+
     params = listOf(
+
         SubmittableExtrinsicV28.toHexString(extrinsic)
+
     )
+
 )
